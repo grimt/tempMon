8854e5b70690a70ce988b3d3ad85d07a1951e707
Graeme Thomson <graeme.thomson@alliedtelesis.co.nz>
2018-07-31 12:34:08 +1200

    Add new file to read temperature on the pi2
    Save it to two files. One is always overwritten with the latest
    temperature, the otther appends the data.
    
    Also remove library write to file code that
    uses previous projects absolute paths.

---
+++ p2_dht_read_save_temp.py
@@ -0,0 +1,43 @@
+# Read temp from DHT
+# Add local time whem temp was Read
+# Save data to a failed
+# Append data to an archive
+
+def p2AppendAmbTempToFile (temp):
+    # append the temperature to a file to allow analysis of tempereture changes.
+    localDate = datetime.datetime.date(datetime.datetime.now()).strftime('%d/%m/%Y')
+    localtime = datetime.datetime.time(datetime.datetime.now()).strftime('%H:%M:%S')
+    data = localDate  + ' ' + localtime + ',' + temp + "\n"
+    print data
+    appendDataToFile('../datafiles/p2ArchiveTemperature.txt', data)
+
+
+def p2WriteAmbTempToFile (temp):
+    # Write the temperature to a file. This will be the latest temperature
+    localDate = datetime.datetime.date(datetime.datetime.now()).strftime('%d/%m/%Y')
+    localtime = datetime.datetime.time(datetime.datetime.now()).strftime('%H:%M:%S')
+    data = localDate  + ' ' + localtime + ',' + temp + "\n"
+    print data
+    writeDataToFile('../datafiles/p2CurrentTemperature.txt', data)
+
+def p2ReadFromDHT():
+    while True:
+        #humidity, temperature = Adafruit_DHT.read_retry(22, 4)
+        humidity, temperature = Adafruit_DHT.read(22, 4)
+
+
+        # Note that sometimes you won't get a reading and
+        # the results will be null (because Linux can't
+        # guarantee the timing of calls to read the sensor).
+        # If this happens try again!
+
+        if humidity is not None and temperature is not None:
+            print ('Temp= ' + str(format(temperature,'0.2f')))
+            tempStr = str(format(temperature,'0.2f'))
+            p2AppendAmbTempToFile(tempStr)
+            p2WriteAmbTempToFile(tempStr)
+        else:
+            print ('Failed to get reading. Try again!')
+        time.sleep(2)
+
+p2ReadFromDHT()
